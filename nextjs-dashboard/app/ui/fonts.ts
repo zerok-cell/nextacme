37df413617cb61ad578b5a71159755071ee06889
zerok-cell <swiftkey382@gmail.com>
2024-12-02 19:43:00 +0000

    init

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter } from 'next/font/google';
+import {Lusitana} from "next/font/google";
+
+export const lusitana = Lusitana({subsets:['latin'], weight:['400','700']})
+export const inter = Inter({ subsets: ['latin'] });
